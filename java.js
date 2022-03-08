d584f7b9312e2fffd1818bae3b9a4459b508e71f
huyle <leduchuy2801@gmail.com>
2022-03-08 14:41:25 +0700

    hello

---
+++ java.js
@@ -0,0 +1,15 @@
+function showMessage(){
+    alert('Xin chào!');
+}
+
+//function showMessage(){
+    //document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+//}
+
+// <div id="messageDiv"></div>
+//function showMessage(){
+    //document.write('Xin chào!');
+
+//function showMessage(){
+   // console.log('Xin chào!');
+//}
